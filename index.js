53b178fa5fa21fa39eff26d69a4afd4cb646fbb7
Seckin Tataroglu <seckintataroglu@gmail.com>
2020-05-06 21:08:09 +0100

    vscode

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // Comment
 console.log("Hello World");
 
-let name = "Mosh";
+let name = "Seckin";
 console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen(-)
+// Are case-sensitive
+
+let firstName = "Seckin";
+let lastName = "Tataroglu";
+
+
+let interestRate = 0.3
+interestRate = 1;
+console.log(interestRate)
+
+
+let name = "Seckin"; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literals
+let firstName = undefined;
+let selectedColour = null;
+
